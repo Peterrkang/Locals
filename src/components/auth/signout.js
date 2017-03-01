ac9f171c69bed22866d34e5f4094ae66e29765a0
Peter Kang <Peterrkang@gmail.com>
2017-02-28 22:35:56 -0800

    redirect back to homepage after logout

--- src/components/auth/signout.js
+++ src/components/auth/signout.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 
+
 class SignOut extends Component {
+
   componentWillMount() {
     this.props.signoutUser();
   }
 
   render() {
     return (
-      <div>Successfully Logged Out!</div>
+      <div className="alert alert-success"> Successfully Logged Out! </div>
     )
   }
 
