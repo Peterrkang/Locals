b62fc20c7b857470feef58fcc55ad138577278c3
Peter Kang <Peterrkang@gmail.com>
2017-03-18 15:13:24 -0700

    prevent submit on search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,12 +10,22 @@ export default class SearchBar extends Component {
     this.state = { term: '' };
   }
 
+  handleKeyPress(target) {
+    if(target.charCode==13){
+        target.preventDefault();
+    }
+
+  }
+
   render() {
     return (
-        <input className="form-control"
+        <input
+          className="form-control"
           value = {this.state.term}
+          onFocus = { () => {browserHistory.push('/events');} }
           onChange = {event => this.onInputChange(event.target.value)}
           placeholder="Search Events"
+          onKeyPress = {target => this.handleKeyPress(target)}
         />
 
       );
@@ -24,7 +34,6 @@ export default class SearchBar extends Component {
   onInputChange(term){
     this.setState({term});
     this.props.onSearchTermChange(term);
-    browserHistory.push('/events');
   }
 
 };
